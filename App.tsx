d8d83866bbba5deb43352df5da644d38402a5cb6
owaisahmad870 <owaisahmad870@gmail.com>
2021-08-25 21:14:24 +0500

    cognito lambda trigger

--- App.tsx
+++ App.tsx
@@ -2,12 +2,16 @@ import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import Amplify from 'aws-amplify'
+import config from './src/aws-exports'
+import { withAuthenticator } from 'aws-amplify-react-native'
+Amplify.configure(config);
 
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -22,3 +26,4 @@ export default function App() {
     );
   }
 }
+export default withAuthenticator(App);
